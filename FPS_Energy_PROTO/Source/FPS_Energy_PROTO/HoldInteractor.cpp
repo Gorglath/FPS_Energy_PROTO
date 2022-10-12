f77eca828499903e6b65982703016508ed242de8
idan8426 <idan8426@gmail.com>
2022-10-12 12:50:50 +0300

    Created hold interactor class.

---
+++ FPS_Energy_PROTO/Source/FPS_Energy_PROTO/HoldInteractor.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HoldInteractor.h"
+
+// Sets default values
+AHoldInteractor::AHoldInteractor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	m_smInteractorMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Interactor mesh"));
+	RootComponent = m_smInteractorMeshComponent;
+	
+}
+
+// Called when the game starts or when spawned
+void AHoldInteractor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AHoldInteractor::EndInteraction()
+{
+	if(m_bIsCompleted)
+		return;
+
+	m_fHoldingTimer = 0.0f;
+	m_bIsInteracted = false;
+}
+void AHoldInteractor::ResetInteraction()
+{
+	m_bIsCompleted = false;
+	OnResetInteraction();
+}
+void AHoldInteractor::StartInteraction()
+{
+	OnStartedInteraction();
+	m_bIsInteracted = true;
+}
+
+// Called every frame
+void AHoldInteractor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if(m_bIsCompleted)
+		return;
+	
+	if(!m_bIsInteracted)
+		return;
+
+	m_fHoldingTimer += DeltaTime;
+
+	if(m_fHoldingTimer < m_fTimeToHoldForActivation)
+		return;
+
+	m_bIsCompleted = true;
+	OnCompletedInteraction();
+}
+
---
+++ FPS_Energy_PROTO/Source/FPS_Energy_PROTO/HoldInteractor.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HoldInteractor.generated.h"
+UENUM()
+enum EHoldInteractorType
+{
+	NONE UMETA("NONE"),
+	PYLON UMETA("Energy Pylon"),
+	CHEST UMETA("Energy Chest")
+	
+};
+UCLASS()
+class FPS_ENERGY_PROTO_API AHoldInteractor : public AActor
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* m_smInteractorMeshComponent{nullptr};
+	
+	UPROPERTY(EditAnywhere, Category = "Hold Interactor")
+	float m_fTimeToHoldForActivation{1.0f};
+	
+	UPROPERTY(EditAnywhere, Category = "Hold Interactor")
+	TEnumAsByte<EHoldInteractorType> m_eHoldInteractorType{NONE};
+	//helpers
+	bool m_bIsInteracted{false};
+	bool m_bIsCompleted{false};
+	float m_fHoldingTimer{0.0f};
+public:	
+	// Sets default values for this actor's properties
+	AHoldInteractor();
+
+    //Blueprint callable events.
+	UFUNCTION(BlueprintImplementableEvent, Category = "Hold Interactor")
+	void OnCompletedInteraction();
+	UFUNCTION(BlueprintImplementableEvent, Category = "Hold Interactor")
+	void OnStartedInteraction();
+	UFUNCTION(BlueprintImplementableEvent, Category = "Hold Interactor")
+	void OnResetInteraction();
+
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void StartInteraction();
+
+	virtual void EndInteraction();
+
+	virtual void ResetInteraction();
+	
+	bool GetIsCompleted() const{return m_bIsCompleted;}
+};
